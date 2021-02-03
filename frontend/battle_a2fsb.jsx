b80e0076c4fb70a325606270e3d81eccd1aaf134
kevin park <kevin.park02@gmail.com>
2021-02-02 16:39:41 -0800

    Create store and make getState and dispatch methods available on the window

--- frontend/battle_a2fsb.jsx
+++ frontend/battle_a2fsb.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import configureStore from './store/store'
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+
+  // TESTING - START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // TESTING - END
+
   ReactDOM.render(<h1>Battle a2f SB</h1>, root);
 });
