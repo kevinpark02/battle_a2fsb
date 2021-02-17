00d169fedf2f225aae54c3afc08a1343ee4a3814
kevin park <kevin.park02@gmail.com>
2021-02-16 17:31:36 -0800

    create post ajax requests

---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,22 @@
+export const createPost = (post) => {
+    return $.ajax({
+        url: `/api/posts`,
+        method: "POST",
+        data: { post }
+    });
+};
+
+export const updatePost = (post) => {
+    return $.ajax({
+        url: `/api/posts/${post.id}`,
+        method: "PATCH",
+        data: { post }
+    });
+};
+
+export const deletePost = (postId) => {
+    return $.ajax({
+        url: `/api/posts/${postId}`,
+        method: "DELETE"
+    });
+};
