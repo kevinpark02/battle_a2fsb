22a4bf5e0e1ba2017eca0c805a4e468b64662b22
kevin park <kevin.park02@gmail.com>
2021-02-16 16:46:45 -0800

    create restful routes for post

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :create]
     resources :battles, only: [:index, :show, :create, :update]
     resources :tasks, only: [:create, :update, :delete]
+    resources :posts, only: [:create, :update, :delete]
     resource :session, only: [:create, :destroy]
   end
 end
